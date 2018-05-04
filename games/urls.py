763747a0c507daadb23810d95b92b2c67d5b9efe
Bryan Chavez <nayrb.94@hotmail.com>
2018-05-04 12:06:39 -0700

    Finished api_views

--- games/urls.py
+++ games/urls.py
@@ -7,10 +7,7 @@ urlpatterns = [
     path('games/', views.game_list),
     path('games/<int:pk>/', views.game_detail),
 
-    """
     # **Uncomment if you're planning on using Django 1.11 or lower**
-    
-    url(r'^games/$', views.game_list),
-    url(r'^games/(?P<pk>[0-9]+)/$', views.game_detail),
-    """
+    # url(r'^games/$', views.game_list),
+    # url(r'^games/(?P<pk>[0-9]+)/$', views.game_detail),
 ]
