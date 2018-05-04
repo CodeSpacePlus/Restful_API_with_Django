248bbb9f63450aef8e0d763821558584cd530c47
Bryan Chavez <nayrb.94@hotmail.com>
2018-05-04 11:15:54 -0700

    Created 'game_detail' view

--- games/views.py
+++ games/views.py
@@ -6,3 +6,25 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 from games.models import Game
 from games.serializer import GameSerializer
+
+
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super[JSONRenderer, self].__init__(content, **kwargs)
+
+
+@csrf_exempt
+def game_list(request):
+    if request.method == 'GET':
+        games = Game.objects.all()
+        games_serializer = GameSerializer(games, many=True)
+        return JSONResponse(games_serializer.data)
+    elif request.method == 'POST':
+        game_data = JSONParser().parse(request)
+        game_serializer = GameSerializer(data=game_data)
+        if game_data.is_valid():
+            game_serializer.save()
+            return JSONResponse(game_serializer.data, status=status.HTTP_201_CREATED)
+        return JSONResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
